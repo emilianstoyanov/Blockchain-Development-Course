e2ea81a03ee4f2904e98cd282215c68761242344
emilianstoyanov <emilian.stoyanov@outlook.com>
2023-04-29 13:35:37 +0300

    update contract

--- 2. Solidity and Smart Contracts Fundamentals/contract.sol
+++ 2. Solidity and Smart Contracts Fundamentals/contract.sol
@@ -12,4 +12,20 @@ contract FundRaiser {
         shares += amount;
     }
 
+    // the function returns us our address
+    function whoAmI() public view returns (address) {
+        return msg.sender;
+    }
+
+    // function returns us our contract address
+    function contractAddress() external view returns (address) {
+        return address(this);
+    }
+
+    // the function returns true or false 
+    // we use 'pure' because all the state we read is in this function. We do not touch the state of the contract
+    function istrue() public pure returns (bool) {
+        bool here;
+        return here;
+    }
 }
