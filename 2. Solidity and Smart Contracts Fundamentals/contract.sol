3adb9d609dec0df9a63bf5a9964f50ea076d163d
emilianstoyanov <emilian.stoyanov@outlook.com>
2023-04-29 19:22:44 +0300

    update contract

--- 2. Solidity and Smart Contracts Fundamentals/contract.sol
+++ 2. Solidity and Smart Contracts Fundamentals/contract.sol
@@ -3,22 +3,43 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract FundRaiser {
-    // we say what type the key and value will be
-    mapping(address => uint) public shares;
     // uint > positive number
     // int > negative number
     uint256 public totalShares;
+    // we say what type the key and value will be
+    mapping(address => uint) public shares;
+    address public owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    uint256 public number;
+
        enum Options {
         One,
         Two,
         Three
     }
-    uint256 public number;
+
+    error Unauthorized(string reason);
 
     // the function will only be available externally > external
-    function addShares(address receiver, uint256 amount) external  {
+    // the function adds the shares of a given investor
+    function addShares(address receiver, uint256 amount) external payable  {
+        // if the address is the same as the owner's, no shares should be sent
+        // require(msg.sender == owner, "Now owner");
+
+        // if the address is the same as the owner's, no shares should be sent
+        // if(msg.sender == owner) {
+        //     return;
+        // }
+        // If the address is the same as the owner's, to display a message
+        if(msg.sender == owner) {
+            revert Unauthorized("Now Owner");
+        }
         totalShares += amount;
         shares[receiver] += amount;
+    }
+
+    // the function displays the holdings of a given investor
+    function getShares(address investor) external view returns (uint256) {
+        return shares[investor];
     }
 
     // the function returns us our address
@@ -47,6 +68,11 @@ contract FundRaiser {
     function increase() external returns (uint256) {
         number++;
         return number;
+    }
+
+    // function returns the seconds
+    function getSeconds() external pure returns (uint256) {
+        return 60 minutes;
     }
 
 
