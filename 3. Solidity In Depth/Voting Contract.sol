30b5456214a4959b982b92dda221d2e57a884fb1
emilianstoyanov <emilian.stoyanov@outlook.com>
2023-05-02 19:18:47 +0300

    new func vote

--- 3. Solidity In Depth/Voting Contract.sol
+++ 3. Solidity In Depth/Voting Contract.sol
@@ -5,9 +5,17 @@ contract Voting {
     uint256 public startTime;
     uint256 public endTime;
 
+    event NewVote(address indexed stakeholder, uint256 shares);
+
     constructor(uint256 start, uint256 end) {
         require(start < end, "Invalid period");
         startTime = start;
         endTime = end;
+    }
+
+    // constructor
+    function vote(address stakeholder) external {
+        // vote logic here
+        emit NewVote(stakeholder, shares);
     }
 }
