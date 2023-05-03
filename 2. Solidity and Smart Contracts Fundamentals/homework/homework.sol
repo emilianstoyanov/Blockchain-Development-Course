410dd149c2a06278aa71629a2ca5c5c570633bda
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-05-03 17:22:12 +0300

    update homework

--- 2. Solidity and Smart Contracts Fundamentals/homework/homework.sol
+++ 2. Solidity and Smart Contracts Fundamentals/homework/homework.sol
@@ -8,13 +8,29 @@ pragma solidity 0.8.19;
 contract homeRepairService {    
     address payable owner = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
     address payable client = payable(msg.sender);
+    uint256 public IDStorage;
 
     mapping(uint256 => address) requestOwner;
     mapping(uint256 => uint256) toPay;
+    mapping(uint256 => bool) isPaid;
+
+
+    string public userName;
+
+    mapping(address => uint256) public shares;
+    mapping(uint256 => bool) public verufy;
+    mapping(address => bool) public isAuditor;
+    mapping(uint256 => mapping(address => bool)) public isConfirmed;
+
+
+    uint256 ethereumAmount;
+    uint256 public valueEth;
+    uint256 countOfAuditors = 0;
+
 
 
     // a request from the customer to fix something
-    function addRepairRequest(uint256 ID, string memory description) public {
+    function addRepairRequest(uint256 ID) public {
         requestOwner[ID] = msg.sender;
     }
 
@@ -33,4 +49,39 @@ contract homeRepairService {
         // ethereumAmount = ethereum * 10**18;
         toPay[ID] = amount;
     }
+    
+    // payment for the service
+    function pay(uint256 ID) public payable {
+        // only approved request can be payed
+        require(toPay[ID] !=0, "Not approved");
+        require(msg.value == toPay[ID]);
+
+        isPaid[ID] = true;
+    }
+
+    // here the auditors check that everything has been done correctly
+    function confirm(uint256 ID) public  {
+
+        require(isAuditor[msg.sender], "Not auditor");
+
+        require(isPaid[ID] == true, "Not paid");
+        verufy[ID] = true;
+
+        countOfAuditors++;
+        if(countOfAuditors > 2) {
+            executeRepaiRequest();
+        }
+
+    }
+
+    function executeRepaiRequest() public payable {
+        require(countOfAuditors >= 2, "You cannot transfer coins!");
+        owner.transfer(ethereumAmount);
+    }
+
+    // only then a certain threshold has passed
+    function moneyBack() public payable {
+        client.transfer(ethereumAmount);
+    }
+
 }
