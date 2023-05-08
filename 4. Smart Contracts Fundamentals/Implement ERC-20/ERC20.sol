3bc18021783b9e25ad482e417028b0d9aa34d8c6
emilianstoyanov <emilian.stoyanov@outlook.com>
2023-05-08 12:40:48 +0300

    update ERC-20

--- 4. Smart Contracts Fundamentals/Implement ERC-20/ERC20.sol
+++ 4. Smart Contracts Fundamentals/Implement ERC-20/ERC20.sol
@@ -10,11 +10,30 @@ contract ERC20 is IERC20{
     uint8 public decimals;
     uint256 public totalSupplay = 10000 ether; //or 10000 * 10**18;
 
-
+    
+    mapping(address => mapping(address => uint256)) private _allowances;
     mapping(address => uint256) private _balances;
 
     function balanceOf(address _owner) public view returns (uint256) {
         return _balances[_owner];
     }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_balances[msg.sender] >= _value, "Insufficient balance");
+
+        _balances[msg.sender] -= _value;
+        _balances[_to] += _value;
+
+        // TODO: emit event
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        _allowances[msg.sender][_spender] += _value;
+        
+        return true;
+    }
+
 
 } 
--- 4. Smart Contracts Fundamentals/Implement ERC-20/IERC20.sol
+++ 4. Smart Contracts Fundamentals/Implement ERC-20/IERC20.sol
@@ -9,6 +9,9 @@ interface IERC20 {
     function decimals() external view returns (uint8);
     function totalSupplay() external view returns (uint256);
     function balanceOf(address _owner) external view returns (uint256 balance);
-
+    function transfer(address _to, uint256 _value) external  returns (bool success);
+    // function transferFrom(address _from, address _to, uint256 _value) external  returns (bool success);
+    function approve(address _spender, uint256 _value) external  returns (bool success);
 } 
+
 
