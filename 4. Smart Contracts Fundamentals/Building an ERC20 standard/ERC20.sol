8b82ff690e853df363b040533b23c78133188c66
emilianstoyanov <emilian.stoyanov@outlook.com>
2023-05-08 13:29:23 +0300

    update ERC20

--- 4. Smart Contracts Fundamentals/Building an ERC20 standard/ERC20.sol
+++ 4. Smart Contracts Fundamentals/Building an ERC20 standard/ERC20.sol
@@ -4,36 +4,76 @@ pragma solidity ^0.8.18;
 
 import "./IERC20.sol";
 
-contract ERC20 is IERC20{
+contract ERC20 is IERC20 {
     string public name;
     string public symbol;
     uint8 public decimals;
-    uint256 public totalSupplay = 10000 ether; //or 10000 * 10**18;
+    // uint256 public totalSupplay = 10000 ether; //or 10000 * 10**18;
+    uint256 public totalSupplay;
 
-    
-    mapping(address => mapping(address => uint256)) private _allowances;
     mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    function _mint(address _to, uint256 _value) internal {
+        _balances[_to] += _value;
+        totalSupplay += _value;
+        emit Transfer(address(0), _to, _value);
+    }
 
     function balanceOf(address _owner) public view returns (uint256) {
         return _balances[_owner];
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
         require(_balances[msg.sender] >= _value, "Insufficient balance");
+        _transfer(_to, _value);
 
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
+        _allowances[msg.sender][_spender] += _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool success) {
+        require(
+            _allowances[_from][msg.sender] >= _value,
+            "Insufficient balance"
+        );
+        _allowances[_from][msg.sender] -= _value;
+
+        _transfer(_to, _value);
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+    function _transfer(address _to, uint256 _value) internal {
         _balances[msg.sender] -= _value;
         _balances[_to] += _value;
-
-        // TODO: emit event
-
-        return true;
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        _allowances[msg.sender][_spender] += _value;
-        
-        return true;
+    function allowance(
+        address _owner,
+        address _spender
+    ) external view returns (uint256 remaining) {
+        return _allowances[_owner][_spender];
     }
-
-
-} 
+}
