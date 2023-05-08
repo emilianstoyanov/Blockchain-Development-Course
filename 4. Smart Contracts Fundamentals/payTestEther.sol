a247149e308d2825e041a994f7fc6f2d320dd1cd
emilianstoyanov <emilian.stoyanov@outlook.com>
2023-05-08 11:56:41 +0300

    update payTest and create ERC-20

--- 4. Smart Contracts Fundamentals/payTestEther.sol
+++ 4. Smart Contracts Fundamentals/payTestEther.sol
@@ -1,14 +1,16 @@
-contract Test {
-    address admin = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
 
-    // first send money from other account to admin
+// we send ethers to the contract!!!
+contract Test {
+
+    // first send money from other account to  the contract
     function payTest() external payable {
-        // payable(admin).transfer(msg.value)
     }
 
     // then send the received money from admin to third account
     function test() external {
-        uint256 amount = 500000000000000000000;
+        uint256 amount = 5000000000000000000;
         payable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4).transfer(amount);
     }
 
@@ -16,5 +18,25 @@ contract Test {
         return address(this).balance;
     }
 
-   
-}   
+}
+
+// we send ethers to the admin
+// contract Test {
+//     address admin = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+//     // first send money from other account to admin
+//     function payTest() external payable {
+//         payable(admin).transfer(msg.value);
+//     }
+
+//     // then send the received money from admin to third account
+//     function test() external {
+//         uint256 amount = 5000000000000000000;
+//         payable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4).transfer(amount);
+//     }
+
+//     function getBalance() external view returns (uint256) {
+//         return address(this).balance;
+//     }
+
+// }
