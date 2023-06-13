8d7044e01785d3fa7ee69246f617f49f41b9e881
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-06-13 16:51:55 +0300

    update package and App

--- 9. Basic Smart Contract Interactions/web3/src/App.js
+++ 9. Basic Smart Contract Interactions/web3/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
-    setCount(count + 1);
+    setCount((count) => count + 1);
   }
 
   return (
--- 9. Basic Smart Contract Interactions/web3/src/components/MyButton/MyButton.jsx
+++ 9. Basic Smart Contract Interactions/web3/src/components/MyButton/MyButton.jsx
@@ -1,6 +1,6 @@
 import "./MyButton.css";
 
-import { useState } from "react";
+
 
 export default function MyButton({ count, buttonClicked }) {
     return <button onClick={buttonClicked}> Clicked {count} times </button>
