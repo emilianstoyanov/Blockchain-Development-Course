ef8839ee83c8cfffe96b89abfd56aa3a89af7a33
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-06-13 15:42:25 +0300

    two buttons with a common counter

--- 9. Basic Smart Contract Interactions/web3/src/App.js
+++ 9. Basic Smart Contract Interactions/web3/src/App.js
@@ -2,12 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 import MyButton from './components/MyButton/MyButton';
-
+import { useState } from "react";
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <MyButton/>
-    </div>
+    <>
+      <div className="App">
+        <MyButton buttonClicked={handleClick} count={count} />
+        <MyButton buttonClicked={handleClick} count={count} />
+      </div>
+    </>
   );
 }
 
--- 9. Basic Smart Contract Interactions/web3/src/components/MyButton/MyButton.jsx
+++ 9. Basic Smart Contract Interactions/web3/src/components/MyButton/MyButton.jsx
@@ -2,14 +2,7 @@ import "./MyButton.css";
 
 import { useState } from "react";
 
-export default function MyButton() {
-    
-    const [count, setCount] = useState(0);
-
-    function handleClick() {
-        setCount(count + 1);
-    }
-
-    return <button onClick={handleClick}> Clicked {count} times </button>
+export default function MyButton({ count, buttonClicked }) {
+    return <button onClick={buttonClicked}> Clicked {count} times </button>
 }
 
