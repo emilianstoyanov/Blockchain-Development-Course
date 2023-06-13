6f8121891321bac62ed2d5adbf0de3d23efafd55
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-06-13 15:22:56 +0300

    count clicked button

--- 9. Basic Smart Contract Interactions/web3/src/App.js
+++ 9. Basic Smart Contract Interactions/web3/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import MyButton from './components/MyButton/MyButton';
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <MyButton/>
+    </div>
   );
 }
 
--- 9. Basic Smart Contract Interactions/web3/src/components/MyButton/MyButton.jsx
+++ 9. Basic Smart Contract Interactions/web3/src/components/MyButton/MyButton.jsx
@@ -1,5 +1,15 @@
-function MyButton() {
-    return <button>I'm a button</button>;
+import "./MyButton.css";
+
+import { useState } from "react";
+
+export default function MyButton() {
+    
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return <button onClick={handleClick}> Clicked {count} times </button>
 }
 
-export default  MyButton;
