22b5213c18a73a4c2d72597de7cc9bc5bddc646a
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-06-14 15:27:17 +0300

    block visualization

--- 9. Basic Smart Contract Interactions/web3/src/App.js
+++ 9. Basic Smart Contract Interactions/web3/src/App.js
@@ -6,15 +6,18 @@ import { useEffect, useState } from "react";
 
 import { ethers } from "ethers";
 
+
 function App() {
 
   const [count, setCount] = useState(0);
   const [currentAccount, setCurrentAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [blockNumber, setBlockNumber] = useState(null);
 
 
   useEffect(() => {
     if (localStorage.getItem("connected")) {
-      handleConnection();
+      // handleConnection();
     }
   }, []);
 
@@ -31,24 +34,32 @@ function App() {
 
     // A Web3Provider wraps a standard Web3 provider, which is
     // what MetaMask injects as window.ethereum into each page
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const newProvider = new ethers.providers.Web3Provider(window.ethereum);
 
-    provider
+    newProvider
       .send("eth_requestAccounts", [])
       .then((accounts) => {
         if (accounts.length > 0) setCurrentAccount(accounts[0]);
         localStorage.setItem('connected', true);
+
+        setProvider(newProvider);
       })
       .catch((e) => console.log(e));
 
   }
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  
   function getBlockNumber() {
     // Look up the current block number
-    provider.getBlockNumber();
+    if(!provider) {
+      return;
+    }
+
+    provider.getBlockNumber().then((blockNumber) => {
+      setBlockNumber(blockNumber);
+    });
     // 16987688
- 
   }
+
   return (
     <>
       <div className="App">
@@ -60,6 +71,10 @@ function App() {
       {
         currentAccount ? <h1>{currentAccount}</h1> : <h1>Not connected</h1> 
       }
+      {
+        provider ? (<button onClick={getBlockNumber}>Get Block Number </button>) : (<h1>Not connevted</h1>)
+      }
+      {blockNumber ? <h1>{blockNumber}</h1> : <h1>Not connected</h1>}
     </>
   );
 }
