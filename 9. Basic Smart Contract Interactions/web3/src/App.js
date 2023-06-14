fff3d59fcd85516e757ba7b6e3a0d199b58bcc2a
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-06-14 11:35:12 +0300

    metamask func

--- 9. Basic Smart Contract Interactions/web3/src/App.js
+++ 9. Basic Smart Contract Interactions/web3/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 
 import MyButton from './components/MyButton/MyButton';
 import { useState } from "react";
+
+import { ethers } from "ethers";
+
 function App() {
 
   const [count, setCount] = useState(0);
@@ -11,13 +14,24 @@ function App() {
     setCount((count) => count + 1);
   }
 
+  function handleConnection() {
+    if (!window.ethereum) {
+      alert("install MetaMask");
+      return;
+    }
+  }
+
+// A Web3Provider wraps a standard Web3 provider, which is
+// what MetaMask injects as window.ethereum into each page
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+
   return (
     <>
       <div className="App">
         <MyButton buttonClicked={handleClick} count={count} />
         <MyButton buttonClicked={handleClick} count={count} />
         <button buttonClicked={handleClick}> Connect </button>
-
       </div>
     </>
   );
