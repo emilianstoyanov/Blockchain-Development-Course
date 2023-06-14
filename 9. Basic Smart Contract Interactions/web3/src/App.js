a70b631be7a5dcb90dad8e383776dc13d7a0a9d2
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-06-14 13:22:59 +0300

    update connect wallet

--- 9. Basic Smart Contract Interactions/web3/src/App.js
+++ 9. Basic Smart Contract Interactions/web3/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import MyButton from './components/MyButton/MyButton';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { ethers } from "ethers";
 
@@ -10,6 +10,13 @@ function App() {
 
   const [count, setCount] = useState(0);
   const [currentAccount, setCurrentAccount] = useState(null);
+
+
+  useEffect(() => {
+    if (localStorage.getItem("connected")) {
+      handleConnection();
+    }
+  }, []);
 
   function handleClick() {
     setCount((count) => count + 1);
@@ -20,24 +27,29 @@ function App() {
       alert("install MetaMask");
       return;
     }
+  
+
+    // A Web3Provider wraps a standard Web3 provider, which is
+    // what MetaMask injects as window.ethereum into each page
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    provider
+      .send("eth_requestAccounts", [])
+      .then((accounts) => {
+        if (accounts.length > 0) setCurrentAccount(accounts[0]);
+        localStorage.setItem('connected', true);
+      })
+      .catch((e) => console.log(e));
+
+ 
+    
   }
-
-// A Web3Provider wraps a standard Web3 provider, which is
-// what MetaMask injects as window.ethereum into each page
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-provider
-  .send("eth_requestAccounts", [])
-  .then((accounts) => {
-  if (accounts.length > 0) setCurrentAccount(accounts[0]);
-  }).catch((e) => console.log(e));
-
   return (
     <>
       <div className="App">
         <MyButton buttonClicked={handleClick} count={count} />
         <MyButton buttonClicked={handleClick} count={count} />
-        <button buttonClicked={handleClick}> Connect </button>
+        <button onClick={handleConnection}> Connect </button>
       </div>
       <div></div>
       {
